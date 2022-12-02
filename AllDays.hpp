f4913ccf82238da9e2fb9e370e296893698f53c7
Joakim <joakim@illusionlabs.se>
2022-12-02 16:05:53 +0100

    Include for all days

---
+++ AllDays.hpp
@@ -0,0 +1,14 @@
+//
+//  AllDays.h
+//  AdventOfCode
+//
+//  Created by Joakim on 2022-12-02.
+//
+
+#ifndef AllDays_h
+#define AllDays_h
+
+#include "Day01.hpp"
+#include "Day02.hpp"
+
+#endif /* AllDays_h */
